4ce7222ebca4204c8ac4db86624b90795b344227
NoahMcGraw <McMoldy96@gmail.com>
2022-01-28 00:49:08 -0600

    Created frontend app for showing tasks

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,5 +1,7 @@
 const TodoList = artifacts.require("./TodoList.sol");
+const Account = artifacts.require("./Account.sol");
 
 module.exports = function(deployer) {
   deployer.deploy(TodoList);
+  deployer.deploy(Account);
 };
