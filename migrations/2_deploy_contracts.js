0579bf4dd80b7a5727340bbdcefba77f8e50e676
NoahMcGraw <McMoldy96@gmail.com>
2022-02-07 01:13:27 -0600

    Removed unrelated files

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,7 +1,5 @@
 const TodoList = artifacts.require("./TodoList.sol");
-const Account = artifacts.require("./Account.sol");
 
 module.exports = function(deployer) {
   deployer.deploy(TodoList);
-  deployer.deploy(Account);
 };
